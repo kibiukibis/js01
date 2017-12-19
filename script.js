07a09d13c2919208eea0e2dc2142375c330500df
kibiukibis <kibiukibis@gmail.com>
2017-12-19 10:29:49 +0200

    javascript second homework

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-// Šiuo metu Lietuvoje yra 26 laipsniai pagal Farenheitą,
-// naudodamiesi JavaScript 
-// apskaičiuokite kokia temperatūra yra pagal Celsijų.
+// Įmonė nori įsigyti 2 baltus arba juodus tos pačios rūšies kompiuterius
+// (būtinai vienos spalvos),kurių bendra kaina neviršytų 1600 €. 
+// Sugeneruokite sąrašą kompiuterių, kuriuos galite pasiūlyti.
+// Masyvas su objektais:
+// var pcs = [  
+//    { "modelis":"lenovo idėja", "kaina":1234, "spalva":{"raudona":1,"žalia":2} }
+//    { "modelis":"hp monstras", "kaina":800, "spalva":{"juoda":2,"geltona":0} },
+//    { "modelis":"toshiba sriuba", "kaina":256, "spalva":{"mėlyna":3,"žalia":1} },
+//    { "modelis":"dell apskritimas", "kaina":697, "spalva":{"juoda":1,"balta":2} },
+//    { "modelis":"acer peizažas", "kaina":620, "spalva":{"juoda":4,"balta":2} },
+//    { "modelis":"apple 256", "kaina":2560, "spalva":{"balta":3,"juoda":1} },
+//    { "modelis":"asus pokšt", "kaina":1001, "spalva":{"juoda":2,"geltona":3} }
+// ];
 // Būtinos sąlygos:
-// - Turime "string" kintamąjį su tekstu "Lietuvoje šiuo metu laipsnių pagal Celsijų"
-// - Negalima rašyti jokio papildomo teksto
-// - Temperatūra pagal Celsijų turi būti nurodyta vienas skaičius po kablelio
-// - Temperatūra turi būti konvertuojama iš Farenheito į Celsijų
-// - Rezultatą išvesti konsolėje
-// Rezultatas:
-// Lietuvoje šiuo metu -3.3 laipsnių pagal Celsijų
+// - Panaudokite bent po vieną ciklą ir sąlyginį sakinį,
+// - Rezultatas išvedamas konsolėje,
+// - Line break naudokite "\n".
+// Rezultatas turėtų atrodyti taip:
+// Galimi variantai:
+// Modelis: hp monstras Kaina: 1600 Spalvos: juoda
+// Modelis: dell apskritimas Kaina: 1394 Spalvos: balta
+// Modelis: acer peizažas Kaina: 1240 Spalvos: balta ir juoda
 
 
-var farenheit = 26;
-var str = "Lietuvoje šiuo metu laipsnių pagal Celsijų";
 
-function toCelsius() {
-    return ((farenheit - 32) * 5 / 9).toFixed(1);
-}
-var first = str.substring(0, 20);
-var last = str.substring(19);
-console.log(first + toCelsius() + last);
+var pcs = [
+    { "modelis": "lenovo idėja", "kaina": 1234, "spalva": { "raudona": 1, "žalia": 2 } },
+    { "modelis": "hp monstras", "kaina": 800, "spalva": { "juoda": 2, "geltona": 0 } },
+    { "modelis": "toshiba sriuba", "kaina": 256, "spalva": { "mėlyna": 3, "žalia": 1 } },
+    { "modelis": "dell apskritimas", "kaina": 697, "spalva": { "juoda": 1, "balta": 2 } },
+    { "modelis": "acer peizažas", "kaina": 620, "spalva": { "juoda": 4, "balta": 2 } },
+    { "modelis": "apple 256", "kaina": 2560, "spalva": { "balta": 3, "juoda": 1 } },
+    { "modelis": "asus pokšt", "kaina": 1001, "spalva": { "juoda": 2, "geltona": 3 } }
+];
+
+var textOut = "";
+pcs.forEach(model => {
+    var spalvos;
+    if (model.kaina <= 800 && (model.spalva.juoda || model.spalva.balta) && (model.spalva.juoda >= 2 || model.spalva.balta >= 2)) {
+        if (model.spalva.juoda) spalvos = "juoda";
+        if (model.spalva.balta) spalvos = "balta";
+        if (model.spalva.juoda && model.spalva.balta) spalvos = "balta ir juoda";
+        textOut += `Modelis: ${model.modelis} Kaina: ${model.kaina*2} Spalvos: ${spalvos}\n`;
+    }
+});
+console.log(`Galimi variantai:\n${textOut}`)

c65c51cbbc3afb9bbd7ea3f19e81e3adbbc7df3e
kibiukibis <kibiukibis@gmail.com>
2017-12-20 10:04:06 +0200

    javaScript 3 homework

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-// Įmonė nori įsigyti 2 baltus arba juodus tos pačios rūšies kompiuterius
-// (būtinai vienos spalvos),kurių bendra kaina neviršytų 1600 €. 
-// Sugeneruokite sąrašą kompiuterių, kuriuos galite pasiūlyti.
-// Masyvas su objektais:
-// var pcs = [  
-//    { "modelis":"lenovo idėja", "kaina":1234, "spalva":{"raudona":1,"žalia":2} }
-//    { "modelis":"hp monstras", "kaina":800, "spalva":{"juoda":2,"geltona":0} },
-//    { "modelis":"toshiba sriuba", "kaina":256, "spalva":{"mėlyna":3,"žalia":1} },
-//    { "modelis":"dell apskritimas", "kaina":697, "spalva":{"juoda":1,"balta":2} },
-//    { "modelis":"acer peizažas", "kaina":620, "spalva":{"juoda":4,"balta":2} },
-//    { "modelis":"apple 256", "kaina":2560, "spalva":{"balta":3,"juoda":1} },
-//    { "modelis":"asus pokšt", "kaina":1001, "spalva":{"juoda":2,"geltona":3} }
-// ];
-// Būtinos sąlygos:
-// - Panaudokite bent po vieną ciklą ir sąlyginį sakinį,
-// - Rezultatas išvedamas konsolėje,
-// - Line break naudokite "\n".
-// Rezultatas turėtų atrodyti taip:
-// Galimi variantai:
-// Modelis: hp monstras Kaina: 1600 Spalvos: juoda
-// Modelis: dell apskritimas Kaina: 1394 Spalvos: balta
-// Modelis: acer peizažas Kaina: 1240 Spalvos: balta ir juoda
+// Turime masyvą, kuriame yra trumpos tekstinės eilutės (pateikta apačioje). 
+// Mūsų užduotis:
+
+// 1. Sukurti funkciją, kuri rastų ilgiausią žodį tekstinėje eilutėje
+
+// 2. Panaudojant šią funkciją išvesti kiekvienos eilutės ilgiausią žodį 
+// (rezultatas turi būti viena tekstinė eilutė)
+// 3. Išvedant žodžius panaudoti tarpą tarp žodžių
+
+// 4. Išvedimas turi būti dinamiškas 
+// (pasikeitus masyve esančių eilučių skaičiui - vis tiek gautume kiekvienos eilutės ilgiausią žodį)
+
+// 5. Rezultatą išvesti konsolėje
 
 
+// // Masyvas
+// var posts = ["Sveikinu radus darbą", "Ar tikrai pabaigus kursą viskas bus gerai",
+//  "Javascript nėra Java", "Rasti video medžiagą visada šaunu"];
 
-var pcs = [
-    { "modelis": "lenovo idėja", "kaina": 1234, "spalva": { "raudona": 1, "žalia": 2 } },
-    { "modelis": "hp monstras", "kaina": 800, "spalva": { "juoda": 2, "geltona": 0 } },
-    { "modelis": "toshiba sriuba", "kaina": 256, "spalva": { "mėlyna": 3, "žalia": 1 } },
-    { "modelis": "dell apskritimas", "kaina": 697, "spalva": { "juoda": 1, "balta": 2 } },
-    { "modelis": "acer peizažas", "kaina": 620, "spalva": { "juoda": 4, "balta": 2 } },
-    { "modelis": "apple 256", "kaina": 2560, "spalva": { "balta": 3, "juoda": 1 } },
-    { "modelis": "asus pokšt", "kaina": 1001, "spalva": { "juoda": 2, "geltona": 3 } }
-];
+var posts = ["Sveikinu radus darbą", "Ar tikrai pabaigus kursą viskas bus gerai", "Javascript nėra Java", "Rasti video medžiagą visada šaunu"];
 
-var textOut = "";
-pcs.forEach(model => {
-    var spalvos = "";
-    if (model.kaina <= 800 && (model.spalva.juoda || model.spalva.balta) && (model.spalva.juoda >= 2 || model.spalva.balta >= 2)) {
-        if (model.spalva.juoda >= 2) spalvos = "juoda";
-        if (model.spalva.balta >= 2) spalvos = "balta";
-        if (model.spalva.juoda >= 2 && model.spalva.balta >= 2) spalvos = "balta ir juoda";
 
-        textOut += `Modelis: ${model.modelis} Kaina: ${model.kaina*2} Spalvos: ${spalvos}\n`;
-    }
+function longestWord(sen) {
+    bigWord = "";
+    words = sen.split(" ");
+    words.forEach(function(word) {
+        if (word.length > bigWord.length) {
+            bigWord = word
+        };
+    });
+    return bigWord
+};
+
+
+var newPost = "";
+posts.forEach(longest => {
+    longestWord(longest);
+    newPost += `${bigWord} `;
 });
-console.log(`Galimi variantai:\n${textOut}`)
+console.log(newPost);
